d3e9d437f8a6993cc55627367dff18996cb00121
Daksh Gaur <83418141+warlock-spell@users.noreply.github.com>
2023-02-06 12:06:21 +0530

    add post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
+
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    excerpt = models.CharField(max_length=300)
+    image_name = models.CharField(max_length=300)
+    date = models.DateField()
+    content = models.TextField(validators=[MinLengthValidator(10)])
+    slug = models.SlugField(unique=True, db_index=True)
+
+    def __str__(self):
+        return self.title

fcfda1f7a64760d1a9953cfebf7042efc63cb1cd
Daksh Gaur <83418141+warlock-spell@users.noreply.github.com>
2023-02-06 13:18:59 +0530

    add admin configuration and migrations

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Post, Author, Tag
+
 
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "date", "author")
+    list_filter = ("date", "author", "tags")
+    search_fields = ("title", "content")
+    prepopulated_fields = {"slug": ("title",)}
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Author)
+admin.site.register(Tag)

c7857fd99cd804eeac549481a5e4eea830120119
Daksh Gaur <83418141+warlock-spell@users.noreply.github.com>
2023-02-06 12:20:46 +0530

    add tag model and its relationships

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,13 @@ from django.core.validators import MinLengthValidator
 
 # Create your models here.
 
+class Tag(models.Model):
+    caption = models.CharField(max_length=25, unique=True)
+
+    def __str__(self):
+        return self.caption
+
+
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
@@ -21,6 +28,7 @@ class Post(models.Model):
     content = models.TextField(validators=[MinLengthValidator(10)])
     slug = models.SlugField(unique=True, db_index=True)
     author = models.ForeignKey(Author, on_delete=models.SET_NULL, related_name="posts")
+    tags = models.ManyToManyField("Tag")
 
     def __str__(self):
         return self.title

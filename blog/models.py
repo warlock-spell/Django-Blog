e19febbf3bb095649d05bc9093c7bd88e7829002
Daksh Gaur <83418141+warlock-spell@users.noreply.github.com>
2023-02-06 12:09:57 +0530

    add author model

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,15 @@ from django.core.validators import MinLengthValidator
 
 # Create your models here.
 
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100, unique=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
 class Post(models.Model):
     title = models.CharField(max_length=100)
     excerpt = models.CharField(max_length=300)

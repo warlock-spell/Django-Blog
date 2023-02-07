b0ac47f0cb9c2bf389783174daf52345ce037240
Daksh Gaur <83418141+warlock-spell@users.noreply.github.com>
2023-02-07 15:42:47 +0530

    add comment model

--- blog/models.py
+++ blog/models.py
@@ -32,3 +32,10 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    user_name = models.CharField(max_length=100)
+    user_email = models.EmailField(max_length=100)
+    text = models.TextField(max_length=400)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
